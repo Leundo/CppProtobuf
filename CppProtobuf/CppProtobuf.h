56e3d3491a78f480c633e509aa90478f85c82600
Leundo <daysundoing@outlook.com>
2024-03-13 23:11:07 +0800

    Initial Commit

---
+++ CppProtobuf/CppProtobuf.h
@@ -0,0 +1,18 @@
+//
+//  CppProtobuf.h
+//  CppProtobuf
+//
+//  Created by Undo Hatsune on 2024/03/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CppProtobuf.
+FOUNDATION_EXPORT double CppProtobufVersionNumber;
+
+//! Project version string for CppProtobuf.
+FOUNDATION_EXPORT const unsigned char CppProtobufVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CppProtobuf/PublicHeader.h>
+
+
